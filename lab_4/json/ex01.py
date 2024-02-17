2467ac18d2528acda339eba216c1a853a6178a89
Abdykadyr Daryn <d_abdykadyr@kbtu.kz>
2024-02-18 00:20:43 +0600

    json_changes

--- lab_4/json/ex01.py
+++ lab_4/json/ex01.py
@@ -1,12 +1,9 @@
 import json
-heading = '''
-Interface Status
-================================================================================
-DN                                                 Description           Speed    MTU  
--------------------------------------------------- --------------------  ------  ------'''
+heading =  'Interface Status\n{}\n{:50} {:21} {:8} {:4}\n{} {}  {}  {}'.format('='*80, 'DM' ,'Description', 'Speed', 'MTU', '-'*50, '-'*20, '-'*6, '-'*6)
 
 with open('sample-data.json') as f:
     main_data = json.load(f)
     print(heading)
     for i in main_data["imdata"]:
-        print(f'{i["l1PhysIf"]["attributes"]["dn"]:72}{i["l1PhysIf"]["attributes"]["speed"]:10}{i["l1PhysIf"]["attributes"]["mtu"]}')
+        mn = i["l1PhysIf"]["attributes"]
+        print('{:50} {:20} {:8}  {}'.format(mn["dn"], mn["descr"], mn["speed"], mn["mtu"]))
