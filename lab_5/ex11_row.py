3114fcf9270538b18563a3d24b10ab40f376a741
Abdykadyr Daryn <d_abdykadyr@kbtu.kz>
2024-02-25 02:00:17 +0600

    lab_5.3

--- lab_5/ex11_row.py
+++ lab_5/ex11_row.py
@@ -8,16 +8,14 @@ pattern = r'\n(?P<order>[0-9]+\.)\n(?P<name>.+)\n(?P<count>.+) x (?P<price>.+)\n
 x = re.finditer(pattern, row)
 
 tof = lambda s: float(re.sub('[\s,]', lambda a: '.' if a.group()==',' else '', s))
-# for i in x:
-#     print(i.group('name'))
+
 with open('row_data.csv', 'w', newline='', encoding='utf8') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(['order', 'name', 'count', 'price', 'cost', 'worth'])
     for i in x:
-        rrr = 'Чистая линия скраб мягкий 50 мл'
         writer.writerow([
             i.group('order'), 
-            rrr, 
+            i.group('name'), 
             f'{tof(i.group('count'))}*{tof(i.group('price'))}', 
             tof(i.group('price2')), 
             i.group('cost'), 
