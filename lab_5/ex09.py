7c3b2c885274890e11d9c8efa9173b3e5028e7e8
Abdykadyr Daryn <d_abdykadyr@kbtu.kz>
2024-02-25 01:46:54 +0600

    lab_5.2

--- lab_5/ex09.py
+++ lab_5/ex09.py
@@ -1,4 +1,13 @@
 import re
-l_str = 'HiMyNameIsDarynIt\'sMy 8th ex'
-x = re.sub('[A-Z]', lambda a:f' {a.group()}', l_str)
-print(x)
+
+def test(pattern, text, testnum, result):
+    res = re.sub(pattern, r'\1 \2', text)
+    print(res)
+    if res == result:
+        print(f'Test {testnum} passed!')
+    else:
+        print(f'Test {testnum} didn\'t pass')
+
+pattern = r'(?P<low>\w)(?P<upp>[A-Z])'
+test(pattern, "MySuperTest", 1, "My Super Test")
+test(pattern, " MySuperTest IAmRobot", 2, " My Super Test I Am Robot")
