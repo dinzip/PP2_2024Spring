fc818e736eebd3d2e3fc4c8ced6efd134a6bf3b2
Abdykadyr Daryn <d_abdykadyr@kbtu.kz>
2024-02-25 01:51:29 +0600

    Revert "lab_5.2"
    This reverts commit 7c3b2c885274890e11d9c8efa9173b3e5028e7e8.

--- lab_5/ex07.py
+++ lab_5/ex07.py
@@ -1,12 +1,5 @@
 import re
 
-def test(pattern, text, testnum, result):
-    res = re.sub(pattern, lambda a: a.group('ch').upper(), text)
-    print(res)
-    if res == result:
-        print(f'Test {testnum} passed!')
-    else:
-        print(f'Test {testnum} didn\'t pass')
-
-pattern = r'_(?P<ch>.)'
-test(pattern, 'l_str_fdew f_Re_ds_dop', 1, 'lStrFdew fReDsDop')
+l_str = 'l_str_fdew f_re_ds_dop'
+x = re.sub(r'_.', lambda a: a.group()[1].upper(), l_str)
+print(x)
