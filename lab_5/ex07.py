a8e2a72518aeb0392d68ea4a67e6a719c864a617
Abdykadyr Daryn <d_abdykadyr@kbtu.kz>
2024-02-25 02:02:19 +0600

    lab_5.3

--- lab_5/ex07.py
+++ lab_5/ex07.py
@@ -1,5 +1,12 @@
 import re
 
-l_str = 'l_str_fdew f_re_ds_dop'
-x = re.sub(r'_.', lambda a: a.group()[1].upper(), l_str)
-print(x)
+def test(pattern, text, testnum, result):
+    res = re.sub(pattern, lambda a: a.group('ch').upper(), text)
+    print(res)
+    if res == result:
+        print(f'Test {testnum} passed!')
+    else:
+        print(f'Test {testnum} didn\'t pass')
+
+pattern = r'_(?P<ch>.)'
+test(pattern, 'l_str_fdew f_Re_ds_dop', 1, 'lStrFdew fReDsDop')
